7abbd47477aaf82bc9af0b68f8904334de12adfb
Ismael Santiago <ismaelse95@gmail.com>
2018-02-01 09:58:50 +0100

    Modificado variables

--- LM/boletin_8/bol8ejer1.py
+++ LM/boletin_8/bol8ejer1.py
@@ -2,5 +2,4 @@ from funciones import calcular_coste,pasar_a_segundos,convertir_a_euros
 
 tarifa=int(input("Dame la tarifa por segundos en centimos: "))
 comun=int(input("Dame el número de comunicaciones realizadas: "))
-print("Ahora introducimos la duracion de la llamada con el formato HH:MM:SS")
-duracion=input("¿Cuanto ha durado la comunicación? ")
+
