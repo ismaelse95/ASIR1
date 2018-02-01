ea6545ab7ddd90cf4dca4c6e80f8254f0b54fbf8
Ismael Santiago <ismaelse95@gmail.com>
2018-02-01 09:48:18 +0100

    Añadida funcion calcular coste

--- LM/boletin_8/funciones.py
+++ LM/boletin_8/funciones.py
@@ -4,7 +4,8 @@ def pasar_a_segundos(hora,minu,segun):
 	else:
 		return (hora*3600)+(minu*60)+segun
 
-def calcular_coste:
+def calcular_coste(tarifa, segun):
+	return int(tarifa) * segun
 
 def convertir_a_euros:
 
