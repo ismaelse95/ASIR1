91df286b9e7bf2778012dab926a0bcf0f6b732fd
Ismael Santiago <ismaelse95@gmail.com>
2018-02-01 09:33:02 +0100

    Añadida funcion pasar a segundos

--- LM/boletin_8/funciones.py
+++ LM/boletin_8/funciones.py
@@ -1,4 +1,8 @@
-def pasar_a_segundos:
+def pasar_a_segundos(hora,minu,segun):
+	if hora > 24 or minu > 59 or segun > 59:
+		return 0
+	else:
+		return (hora*3600)+(minu*60)+segun
 
 def calcular_coste:
 
