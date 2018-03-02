fb3ca2e15990345194b49a1775090dffa89c1d81
Ismael Santiago <ismaelse95@gmail.com>
2018-03-02 08:20:54 +0100

    Fichero modificado

--- LM/temperaturas/temperatura.py
+++ LM/temperaturas/temperatura.py
@@ -2,11 +2,11 @@ from funciones import poblacion, url, temperatura
 from lxml import etree
 import time
 
-dia_hoy=time.strftime("%Y-%m-%d")
+dia=time.strftime("%Y-%m-%d")
 muni=input("Dame una población de Sevilla: 	")
 print(" ")
 arbol=etree.parse('sevilla.xml')
-arboltemp=(url(poblacion(arbol,muni)))
-temperaturas=temperatura(arboltemp,dia_hoy)
+arbol2=etree.parse(url(poblacion(arbol,muni)))
+temperaturas=temperatura(arbol2,dia_hoy)
 print("La temperatura máxima de {} es de {}º".format(muni,temperaturas[0]))
 print("La temperatura mínima de {} es de {}º".format(muni,temperaturas[1]))
